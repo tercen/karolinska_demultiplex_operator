4ad85aae2ae042127ca6bfec9366ab7843e0bd8a
fcadete <filipe@cadete.eu>
2022-02-24 22:02:33 +0000

    Checks for input folder and files.

--- main.R
+++ main.R
@@ -4,24 +4,47 @@ library(stringr)
 
 ctx <- tercenCtx()
 
-# Define input and output paths
+# Define input path
 input_path <- "/var/lib/tercen/share/read/files_to_demultiplex"
+
+# Check if a "files_to_demultiplex" folder exists and is not empty
+if( dir.exists(input_path) == FALSE) {
+
+  stop("ERROR: files_to_demultiplex folder does not exist in project write folder.")
+
+}
+
+if (length(dir(input_path)) == 0) {
+  stop("ERROR: files_to_demultiplex folder is empty.")
+}
+
+# Define and create output paths
 
 output_path <- "/var/lib/tercen/share/write/demultiplexed_fastqs/"
 
 system(paste("mkdir", output_path))
 
+# Check if individual files are present in the input folder
+
 r1_file <- list.files(input_path, "_R1_", recursive = TRUE,
                       full.names = TRUE)
+
+if (length(r1_file) == 0) stop("ERROR: could not find a forward read file.")
 
 r2_file <- list.files(input_path, "_R2_", recursive = TRUE,
                       full.names = TRUE)
 
+if (length(r2_file) == 0) stop("ERROR: could not find a reverse read file."
+
 col_file <- list.files(input_path, "col.txt", recursive = TRUE,
                        full.names = TRUE)
 
+if (length(col_file) == 0) stop("ERROR: could not find a col.txt file."
+
 row_file <- list.files(input_path, "row.txt", recursive = TRUE,
                        full.names = TRUE)
+
+if (length(row_file) == 0) stop("ERROR: could not find a row.txt file."
 
 
 print(paste0("python3 demultiplex_TCR_fastqs_by_row_and_column_barcodes.py ",
